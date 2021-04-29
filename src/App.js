3a589850e9dc0a17a6cfb1ae92e73a1ce9a70e15
Redcarlos <39527912+Redcarlos@users.noreply.github.com>
2021-04-28 19:06:22 -0500

    d

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="container mt-5">
-      <h1>hola mundo</h1>
+      <h1>Tareas</h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Nombre de la tarea</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+              <button className="btn btn-warning btn-sm float-right">Editar</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+        </div>
+      </div>
     </div>
   );
 }
